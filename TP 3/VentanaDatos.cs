895ff744858e1a78a8c0a41ba5508faa18a70199
Maria Guadalupe Esteban <86774@sistemas.frc.utn.edu.ar>
2024-05-16 11:35:11 -0300

    tabla de frecuencias corregida

--- TP 3/VentanaDatos.cs
+++ TP 3/VentanaDatos.cs
@@ -126,51 +126,52 @@ namespace TP_3
             DataTable table = new DataTable();
 
             table.Columns.Add("Cantidad de obreros ausentes", typeof(string));
-            table.Columns.Add($"Frecuencia de ausencia en días en {cantSemanas} semanas", typeof(int));
+            table.Columns.Add($"Frecuencia de ausencia en días en 22 semanas", typeof(int));
             table.Columns.Add("Frecuencia en %", typeof(double));
             table.Columns.Add("Frecuencia acumulada en %", typeof(double));
 
-            double cantidadDias = cantSemanas * 5;
+            // 22 semanas * 5 dias laborables
+            double cantidadDias = 22 * 5;
 
 
             DataRow row1 = table.NewRow();
             row1["Cantidad de obreros ausentes"] = "0";
-            row1[$"Frecuencia de ausencia en días en {cantSemanas} semanas"] = (int)Math.Round(cantidadDias * (ausentesCero/100));
+            row1[$"Frecuencia de ausencia en días en 22 semanas"] = (int)Math.Round(cantidadDias * (ausentesCero/100));
             row1["Frecuencia en %"] = ausentesCero;
             row1["Frecuencia acumulada en %"] = ausentesCero;
             table.Rows.Add(row1);
 
             DataRow row2 = table.NewRow();
             row2["Cantidad de obreros ausentes"] = "1";
-            row2[$"Frecuencia de ausencia en días en {cantSemanas} semanas"] = (int)Math.Round(cantidadDias * (ausentesUno/100));
+            row2[$"Frecuencia de ausencia en días en 22 semanas"] = (int)Math.Round(cantidadDias * (ausentesUno/100));
             row2["Frecuencia en %"] = ausentesUno;
             row2["Frecuencia acumulada en %"] = ausentesCero + ausentesUno;
             table.Rows.Add(row2);
 
             DataRow row3 = table.NewRow();
             row3["Cantidad de obreros ausentes"] = "2";
-            row3[$"Frecuencia de ausencia en días en {cantSemanas} semanas"] = (int)Math.Round(cantidadDias * (ausentesDos/100));
+            row3[$"Frecuencia de ausencia en días en 22 semanas"] = (int)Math.Round(cantidadDias * (ausentesDos/100));
             row3["Frecuencia en %"] = ausentesDos;
             row3["Frecuencia acumulada en %"] = ausentesCero + ausentesUno + ausentesDos;
             table.Rows.Add(row3);
 
             DataRow row4 = table.NewRow();
             row4["Cantidad de obreros ausentes"] = "3";
-            row4[$"Frecuencia de ausencia en días en {cantSemanas} semanas"] = (int)Math.Round(cantidadDias * (ausentesTres/100));
+            row4[$"Frecuencia de ausencia en días en 22 semanas"] = (int)Math.Round(cantidadDias * (ausentesTres/100));
             row4["Frecuencia en %"] = ausentesTres;
             row4["Frecuencia acumulada en %"] = ausentesCero + ausentesUno + ausentesDos + ausentesTres;
             table.Rows.Add(row4);
 
             DataRow row5 = table.NewRow();
             row5["Cantidad de obreros ausentes"] = "4";
-            row5[$"Frecuencia de ausencia en días en {cantSemanas} semanas"] = (int)Math.Round(cantidadDias * (ausentesCuatro/100));
+            row5[$"Frecuencia de ausencia en días en 22 semanas"] = (int)Math.Round(cantidadDias * (ausentesCuatro/100));
             row5["Frecuencia en %"] = ausentesCuatro;
             row5["Frecuencia acumulada en %"] = ausentesCero + ausentesUno + ausentesDos + ausentesTres + ausentesCuatro;
             table.Rows.Add(row5);
 
             DataRow row6 = table.NewRow();
             row6["Cantidad de obreros ausentes"] = "5";
-            row6[$"Frecuencia de ausencia en días en {cantSemanas} semanas"] = (int)Math.Round(cantidadDias * (ausentesCinco/100));
+            row6[$"Frecuencia de ausencia en días en 22 semanas"] = (int)Math.Round(cantidadDias * (ausentesCinco/100));
             row6["Frecuencia en %"] = ausentesCinco;
             row6["Frecuencia acumulada en %"] = ausentesCero + ausentesUno + ausentesDos + ausentesTres + ausentesCuatro + ausentesCinco;
             table.Rows.Add(row6);
--- TP 3/obj/Debug/net8.0-windows/TP 3.AssemblyInfo.cs
+++ TP 3/obj/Debug/net8.0-windows/TP 3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP 3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7f54a93dcd61a8466716ca21ce47039df803a26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f957e9c639048a1bbe08ae39e021570a32b8b819")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP 3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP 3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
